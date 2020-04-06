9847319dca73aeb3db1bb3035bce88053285e9ea
jcoops22 <jcooper232074@gmail.com>
2020-04-06 12:24:37 -0600

    updated some broken settings

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,4 +1,3 @@
-console.log("portfolio");
 const frame = document.querySelector("#frame");
 const frameH1 = document.querySelector("#frame h1");
 const scrollLabel = document.querySelector("#scrollLabel");
@@ -15,40 +14,40 @@ var currentProject = 0;
 
 var projects = [
   {
-    vid: "./resources/Project Thumbnails/hbssc",
-    desc: "Local business website. Static website with some scroll animations.",
+    vid: "./resources/Project Thumbnails/hbssc.mov",
+    desc:
+      "Static, local business website. Built with GSAP TweenLite and ScrollMagic Libraries.",
     viewCode: "https://github.com/jcoops22",
-    viewDemo: "https://github.com/jcoops22"
+    viewDemo: "https://github.com/jcoops22",
   },
   {
     vid: "./resources/Project Thumbnails/notebooksc.mov",
     desc:
       "Notebook App. Single page app build with VueJS. Just a project I made to familiarize myself with VueJS.",
     viewCode: "https://github.com/jcoops22/notebookApp",
-    viewDemo: "https://github.com/jcoops22/notebookApp"
+    viewDemo: "https://github.com/jcoops22/notebookApp",
   },
   {
     vid: "./resources/Project Thumbnails/pwdsc.mov",
     desc:
-      "Password manager. Single page app buit to familiarize myself with building and using my own REST API. Uses MongoDB database and NodeJS backend. The rest is just HTMl CSS and Vanilla JS",
+      "Password manager. Single page app built to familiarize myself with building and using my own REST API. Uses MongoDB database and NodeJS backend. The rest is just HTMl CSS and Vanilla JS",
     viewCode: "https://github.com/jcoops22/pwdManager",
-    viewDemo: "https://github.com/jcoops22/pwdManager"
+    viewDemo: "https://github.com/jcoops22/pwdManager",
   },
   {
-    vid: "src",
+    vid: "./resources/Project Thumbnails/drumfinder.mov",
     desc:
-      "Drumfinder App. Single page app. User answers a few quesitons about what they find desireable in a drumset (tones, design, style). Built with VueJS and some ScrollMagic animations.",
+      "Drumfinder App (work in progress). Single page app. User answers a few quesitons about what they find desireable in a drumset(tones, design, style).Built with VueJS and some ScrollMagic animations.",
     viewCode: "https://github.com/jcoops22/drumFinder",
-    viewDemo: "https://github.com/jcoops22/drumFinder"
-  }
+    viewDemo: "https://github.com/jcoops22/drumFinder",
+  },
 ];
 
 frame.addEventListener("scroll", () => {
-  // console.log(frame.scrollTop);
-
   frameH1.style.opacity = `${1 - frame.scrollTop / 177}`;
-  frame.style.backgroundColor = `rgb(${220 -
-    Math.floor(frame.scrollTop * 0.255 * 5.65)},
+  frame.style.backgroundColor = `rgb(${
+    220 - Math.floor(frame.scrollTop * 0.255 * 5.65)
+  },
   ${20 - Math.floor(frame.scrollTop * 0.255 * 5.65)},
   ${60 - Math.floor(frame.scrollTop * 0.255 * 5.65)})`;
   frame.scrollTop > 0
@@ -77,6 +76,9 @@ function reset() {
     frame.style.borderColor = "moccasin";
   }
 }
+window.addEventListener("load", () => {
+  renderProject();
+});
 function renderProject() {
   description.textContent = projects[currentProject].desc;
   viewDemoLink.href = projects[currentProject].viewDemo;
@@ -84,20 +86,20 @@ function renderProject() {
   vid.src = projects[currentProject].vid;
 }
 next.addEventListener("click", () => {
-  if (currentProject >= projects.length) {
+  currentProject++;
+  if (currentProject > projects.length - 1) {
     currentProject = 0;
     renderProject();
   } else {
-    currentProject++;
     renderProject();
   }
 });
 prev.addEventListener("click", () => {
-  if (currentProject <= 0) {
-    currentProject = projects.length;
+  currentProject--;
+  if (currentProject < 0) {
+    currentProject = projects.length - 1;
     renderProject();
   } else {
-    currentProject--;
     renderProject();
   }
 });
